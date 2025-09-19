15c4b2123d571eb624c028025fccf50ff788d05e
abhishek <abhishekk12733@gmail.com>
2025-09-19 17:47:05 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cros from "cros";
+import mongoose from "moongode";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(cros());
+app.use(express.json());
+
+app.use("./authroutes", authroutes);
+app.use("/postroutes", postroutes);
+
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => console.log("mongoose connect"))
+  .catch((err) => console.log(err.message));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
